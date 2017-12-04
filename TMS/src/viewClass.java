4085e6de120d2b3d8b8ef03aaa996f1d51e59a7d
johnstevens14 <jmstevens1899@gmail.com>
2017-12-04 16:47:49 -0600

    Linked faculty page to view student

--- TMS/src/viewClass.java
+++ TMS/src/viewClass.java
@@ -1,3 +1,7 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,10 +15,17 @@
  */
 public class viewClass extends javax.swing.JFrame {
 
+    
+    private ArrayList<Student> students;
     /**
      * Creates new form viewClass
      */
     public viewClass() {
+        initComponents();
+    }
+    
+    public viewClass(String teacher) {
+	students = StudentFile.readStudentsByTeacher(teacher);
         initComponents();
     }
 
