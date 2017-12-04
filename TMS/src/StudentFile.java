23ff40760634cef829eca3f6372fcbaea7933292
johnstevens14 <jmstevens1899@gmail.com>
2017-12-04 13:58:14 -0600

    Refactored because packages were not working

---
+++ TMS/src/StudentFile.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author johnstevens
+ */
+public class StudentFile
+{
+    private static void writeStudents(Student[] students)
+    {
+	ObjectOutputStream out;
+	
+	try
+	{
+	    out = new ObjectOutputStream(new FileOutputStream("students.dat"));
+	    
+	    for(int i = 0; i < students.length; i++)
+	    {
+		out.writeObject(students[i]);
+	    }
+	    out.close();
+	}
+	catch(IOException e)
+	{
+	    System.out.println(e.getMessage());
+	}
+    }
+    
+    private static Student readStudentById(int studentId)
+    {
+	ObjectInputStream in;
+	Student stu;
+	
+	try
+	{
+	    in = new ObjectInputStream(new FileInputStream("students.dat"));
+	    
+	    while(true)
+	    {
+		stu = (Student) in.readObject();
+		if(stu.compareStudentId(studentId))
+		{
+		    return stu;
+		}
+	    }
+	}
+	catch(IOException | ClassNotFoundException e)
+	{
+	    System.out.println(e.getMessage());
+	    return null;
+	}
+    }
+    
+    private static ArrayList<Student> readStudentsByTeacher(String teacher)
+    {
+	ArrayList<Student> students = new ArrayList<Student>();
+	ObjectInputStream in;
+	Student stu;
+	
+	try
+	{
+	    in = new ObjectInputStream(new FileInputStream("students.dat"));
+	    while(true)
+	    {
+		stu = (Student) in.readObject();
+		if(stu.compareTeacher(teacher))
+		    students.add(stu);
+	    }
+	}
+	catch(EOFException eof)
+	{
+	    return students;
+	}
+	catch(IOException | ClassNotFoundException e)
+	{
+	    System.out.println(e.getMessage());
+	    return null;
+	}
+    }
+}
